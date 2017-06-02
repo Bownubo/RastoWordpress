c2564c4ceb09629af311a904962b1acf6bde3ac1
wout <woutvankampen@hotmail.com>
2017-06-02 11:57:13 +0200

    website shit

---
+++ website/over/webdesign.php
@@ -0,0 +1,31 @@
+<?php include '../header.php' ?>
+<div class="container">
+  <div class="col-lg-8 col-md-8 col-sm-10">
+    <h1>Webdesign</h1>
+
+    <p>
+    Een professionele website is een onmisbaar middel in de communicatie met klanten en geïnteresseerden. Ook is het van belang dat uw website goed vindbaar is in de bestaande zoekmachines want u wilt een groot aantal bezoekers. Deze bezoekers zoeken naar actuele informatie en u wilt ze daarmee helpen. Alleen kost het nogal wat tijd om een website up-to-date te houden.
+    </p>
+
+    <p>
+    Daarom maakt het Rasto voor veel van zijn klanten gebruik van een Content Management System (CMS). In het CMS kunt u, wanneer u bekend bent met een tekstverwerker, eenvoudig informatie op de website plaatsen of wijzigen. Heeft u behoefte aan een website, of wilt u gewoon meer informatie over wat het Rasto voor u kan betekenen, neem dan eens <a href="../contact.php">contact</a> met ons op.
+    </p>
+
+    <p>
+    Stagiaires<br/>
+    Wij kunnen voor onze afdeling webdesign en applicatie-ontwikkeling ook stagiaires gebruiken op mbo- en hbo-niveau. Klik hier voor informatie.
+    </p>
+  </div>
+  <div class="col-lg-3 col-md-3 col-sm-6">
+    <div class="sidebar-item">
+      <h1>Contact gegevens</h1>
+      <p></p>
+    </div>
+    <div class="sidebar-item">
+      <h1>Over Rasto</h1>
+      <p><a href="./over/Over-Rasto.php">Over Rasto</a><br/><a href="./over/Webdesign.php">Webdesign</a></p>
+    </div>
+  </div>
+</div>
+</body>
+</html>
