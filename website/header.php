264a46ce2dbaabbf56165a926a6312d492e01f1a
wout <woutvankampen@hotmail.com>
2017-06-02 12:12:41 +0200

    css

--- website/header.php
+++ website/header.php
@@ -7,3 +7,63 @@
 <link rel="stylesheet" href="./css/main.css">
 <title>Rasto-Over-Rasto</title>
 </head>
+<div class="slideshow-container">
+ <div class="mySlides fade">
+   <div class="numbertext">1 / 3</div>
+   <img src="./img/img1.jpg" style="width:100%">
+   <div class="text">Caption Text</div>
+ </div>
+
+ <div class="mySlides fade">
+   <div class="numbertext">2 / 3</div>
+   <img src="./img/img2.jpg" style="width:100%">
+   <div class="text">Caption Two</div>
+ </div>
+
+ <div class="mySlides fade">
+   <div class="numbertext">3 / 3</div>
+   <img src="./img/img3.jpg" style="width:100%">
+   <div class="text">Caption Three</div>
+ </div>
+
+</div>
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script language="JavaScript">
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
